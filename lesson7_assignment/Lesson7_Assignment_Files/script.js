ff1c0e52c0096a488c27ae81fe6333e887fdaa39
Taylor Robbins <tnr15c@my.fsu.edu>
2016-11-16 18:21:15 +0000

    Lesson 7 final commit

---
+++ lesson7_assignment/Lesson7_Assignment_Files/script.js
@@ -0,0 +1,16 @@
+/*
+Program Name: Recipe Display Applicatio
+Author: Taylor Robbins
+Date: 11/14/16
+Filename: script.js
+*/
+//displays the next element after the current target
+function display(event) {
+    
+    $(event.currentTarget).next().fadeIn("slow");
+    
+}//end of display
+
+//attach  event listener to h3 elements to invoke display function when clicked
+$("h3").click(display);
+
